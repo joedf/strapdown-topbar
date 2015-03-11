f2f636bc2dce2ebc33c3f2fdd1fff4c5154f188c
Joe DF <joedf@ahkscript.org>
2015-03-11 13:40:35 -0400

    v1.1.0
    // strapdown-topbar.js v1.1.0
    // Revision date: 12:43 2015-03-11
    // - ADDED menu toggling for Mobile devices
    // - FIXED Known issue : right-version is reversed
    // - Missing feature : support for dropdown menus

--- assets/strapdown/strapdown-topbar.js
+++ assets/strapdown/strapdown-topbar.js
@@ -1,8 +1,10 @@
-﻿// strapdown-topbar.js v1.0.0
+﻿// strapdown-topbar.js v1.1.0
 // by Joe DF, Released under MIT License.
 
-// Revision date: 00:12 2015-03-11
-// Known issue : right-version is reversed
+// Revision date: 12:43 2015-03-11
+// - ADDED menu toggling for Mobile devices
+// - FIXED Known issue : right-version is reversed
+// - Missing feature : support for dropdown menus
 
 /* HTML Original Template
 <topbar>
@@ -13,8 +15,17 @@
 /* HTML Result Template
 <div class="navbar-inner">
 	<div class="container">
+		<a class="btn btn-navbar" data-toggle="collapse" data-target="#navbar-main" id="navbar-main-btn" onclick="nbar_toggle()>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		</a>
 		<div id="headline" class="brand">TITLE</div>
-		<div id="headline-item" class="brand">ITEM_HTML</div>
+		<div class="nav-collapse collapse navbar-responsive-collapse pull-right" id="navbar-main">
+			<ul class="nav navbar-nav navbar-right">
+				<li class="headline-item brand">ITEM_HTML</li>
+			</ul>
+		</div>
 	</div>
 </div>
 */
@@ -25,26 +36,37 @@
 	var topbar_tag = document.getElementsByTagName('topbar')[0];
 	var topbar_items = topbar_tag.getElementsByTagName('item');
 	
-	// Add in our need styles - http://stackoverflow.com/a/707580/883015
+	// Add in our needed styles - http://stackoverflow.com/a/707580/883015
+	var calign = (topbar_tag.hasAttribute('right'))?'right':'left';
 	var css = document.createElement("style");
 	css.type = "text/css";
-	if (topbar_tag.hasAttribute('right')) {
-		css.innerHTML = ".headline-item{float:right !important;text-align:right;font-size:14px !important;}";
-	} else {
-		css.innerHTML = ".headline-item{text-align:right;font-size:14px !important;}";
-	}
+	css.innerHTML = '.headline-item{text-align:right;font-size:14px !important;}'
+				  + '@media(min-width:980px){.nav > li > a {display:inline !important;padding:0 !important;}}'
+				  + '@media(max-width:979px){'
+				  + '#navbar-main{width:100%}.nav > li {float:none !important;padding: 13px 15px 6px !important;display:block;}'
+				  + '.nav > li > a {display:block !important;padding:0 !important;}'
+				  + '#navbar-main ul{float:'+calign+';}'
+				  + '.headline-item{text-align:'+calign+';}}';
 	document.body.appendChild(css);
 	
-	// Insert the title
-	var content = '<div class="navbar-inner"><div class="container"><div id="headline" class="brand">' + doctitle + '</div>';
+	// Add in custom menu-toggle code
+	var menucode = document.createElement("script");
+	menucode.innerHTML = 'function nbar_toggle(){var nbar=document.getElementById("navbar-main");var nbar_t=nbar.className;if(nbar_t.indexOf("collapse")>-1){nbar.className=nbar.className.replace(/collapse/g,"open");}else{nbar.className=nbar.className.replace(/open/g,"collapse");}};';
+	document.body.appendChild(menucode);
+	
+	// Insert the Title and Menu
+	var content = '<div class="navbar-inner"><div class="container"><div id="headline" class="brand">' + doctitle + '</div>'
+				+ '<a class="btn btn-navbar" data-toggle="collapse" data-target="#navbar-main" id="navbar-main-btn" onclick="nbar_toggle()">'
+				+ '<span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></a>'
+				+ '<div class="nav-collapse collapse navbar-responsive-collapse pull-'+calign+'" id="navbar-main"><ul class="nav navbar-nav navbar-right">';
 
 	// Insert the topbar items
 	for (var i = 0; i < topbar_items.length; i++) { 
-		content = content + '<div class="headline-item brand">' + topbar_items[i].innerHTML + '</div>';
+		content = content + '<li class="headline-item brand">' + topbar_items[i].innerHTML + '</li>';
 	}
 	
 	// Finalize
-	content = content + '</div></div>';
+	content = content + '</ul></div></div></div>';
 	topbar.innerHTML = content;
 	
 	// Hide original
